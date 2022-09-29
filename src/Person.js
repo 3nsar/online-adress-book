600758df63cc5fd69c82d785ed23a345dfea07fc
3nsar <84131267+3nsar@users.noreply.github.com>
2022-09-29 20:01:33 +0200

    design change

--- src/Person.js
+++ src/Person.js
@@ -9,8 +9,15 @@ const Person = () => {
                 <h3>First-name: {item.first_name}</h3>
                 <h3>Last-name: {item.last_name}</h3>
                 <h3>Email: {item.email}</h3>
-            </div>
+            </div> 
         ))}
+        {/*<div className='person-container'>
+          <div className='person-content'>
+            <h2><span className='line'>FirstName:</span> Ulrikaumeko</h2>
+            <h2><span className='line'>FirstName:</span> Leao</h2>
+            <h2><span className='line'>Email:</span> leao_ulrikaumeko@u.io</h2>
+          </div>
+        </div>*/}
     </div>
   )
 }
